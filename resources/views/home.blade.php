b8bcdb32e7b2f0687600bc0e5d6324cc0324bf5b
unknown <abibhandari7@gmail.com>
2020-02-19 16:41:12 +0545

    Home Page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,9 +2,16 @@
 @section('content')
  
 {{-- Video Content --}}
-<video autoplay muted loop width="100%">
+{{-- <video autoplay muted loop width="100%">
     <source src="Images/video.mp4" type="video/mp4">    
-</video>
+</video> --}}
+<img src="Images/Background-01.png" width="100%">
 
+<div class="container">
+    <div class="col-md-12">
+        <h2>We Are</h2>
+        <p>lkdsjflkjasdlkfjlkasdjflk</p> 
+    </div>    
+</div>
 
 @endsection
