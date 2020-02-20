5ef22853d1ca2cd3b12d81f03ea19853320e426e
salu <salu.dahal@gmail.com>
2020-02-20 15:47:19 +0545

    Clients worked with

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -2,48 +2,48 @@
 <footer class="section footer-classic context-dark bg-image">
     <div class="container">
         <div class="col-md-12">
-        <div class="row row-30">
-            <div class="col-md-4 col-xl-5">
-                <div class="pr-xl-4"><a class="brand" href="index.html"><img class="brand-logo-light"
-                            src="Images/poon.png" alt="" width="160" height="80"></a>
-                    <p>We are an award-winning creative agency, dedicated to the best result in web design, promotion,
-                        business consulting, and marketing.</p>
+            <div class="row row-30">
+                <div class="col-md-4 col-xl-5">
+                    <div class="pr-xl-4"><a class="brand" href="index.html"><img class="brand-logo-light" src="Images/poon.png" alt="" width="160" height="80"></a>
+                        <p>We are an award-winning creative agency, dedicated to the best result in web design, promotion,
+                            business consulting, and marketing.</p>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <h5>Contacts</h5>
+                    <dl class="contact-list">
+                        <dt>Address:</dt>
+                        <dd>798 South Park Avenue, Jaipur, Raj</dd>
+                    </dl>
+                    <dl class="contact-list">
+                        <dt>email:</dt>
+                        <dd><a href="mailto:#">dkstudioin@gmail.com</a></dd>
+                        <dl class="contact-list">
+                            <dt>phones:</dt>
+                            <dd><a href="tel:#">https://karosearch.com</a> <span>or</span> <a href="tel:#">https://karosearch.com</a>
+                            </dd>
+                        </dl>
+                </div>
+                <div class="col-md-4 col-xl-3">
+                    <h5>Links</h5>
+                    <ul class="nav-list">
+                        </dl>
+                        <li><a href="#">About</a></li>
+                        <li><a href="#">Services</a></li>
+                        <li><a href="#">Contacts</a></li>
+                        <li><a href="#">Career</a></li>
+                    </ul>
                 </div>
             </div>
-            <div class="col-md-4">
-                <h5>Contacts</h5>
-                <dl class="contact-list">
-                    <dt>Address:</dt>
-                    <dd>798 South Park Avenue, Jaipur, Raj</dd>
-                </dl>
-                <dl class="contact-list">
-                    <dt>email:</dt>
-                    <dd><a href="mailto:#">dkstudioin@gmail.com</a></dd>
-                    <dl class="contact-list">
-                        <dt>phones:</dt>
-                        <dd><a href="tel:#">https://karosearch.com</a> <span>or</span> <a
-                                href="tel:#">https://karosearch.com</a>
-                        </dd>
-                    </dl>
-            </div>
-            <div class="col-md-4 col-xl-3">
-                <h5>Links</h5>
-                <ul class="nav-list">
-                    </dl>
-                    <li><a href="#">About</a></li>
-                    <li><a href="#">Services</a></li>
-                    <li><a href="#">Contacts</a></li>
-                    <li><a href="#">Career</a></li>
-                </ul>
-            </div>
         </div>
-    </div>
     </div>
     <div class="row no-gutters social-container">
         <div class="col"><a class="social-inner" href="#"><span class="fa fa-facebook-square fa-3x social"></span><span>Facebook</span></a></div>
         <div class="col"><a class="social-inner" href="#"><span class="fa fa-instagram fa-3x social"></span><span>instagram</span></a></div>
         <div class="col"><a class="social-inner" href="#"><span class="fa fa-google-plus-square fa-3x social"></span><span>google</span></a></div>
     </div>
+    
+   
 </footer>
 
 <!-- js combined -->
@@ -53,6 +53,8 @@
 <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 <script src="https://code.jquery.com/jquery-3.3.1.js"></script>
 <script src="js/style.js"></script>
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 </body>
 
 </html>
